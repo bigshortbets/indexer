2577049813d5e909b8ef5f9ddb360d72ec511920
maciek.nabialek <maciej.nabialek@blockydevs.com>
2023-10-18 16:05:10 +0200

    Migration, last commit

--- src/utils/encodersUtils.ts
+++ src/utils/encodersUtils.ts
@@ -17,3 +17,21 @@ export const encodeMarketTicker = (inputMarketTicker : string) => {
     const buffer = Buffer.from(hexMarketTicker, 'hex');
     return buffer.toString('utf8');
 }
+
+export const convertStringValueToHexBigEndian = (value : string) => {
+    const bigIntValue = BigInt(value);
+    let littleEndianHex = bigIntValue.toString(16);
+    if (littleEndianHex.length % 2 !== 0) {
+        littleEndianHex = '0' + littleEndianHex;
+    }
+    const chunks = littleEndianHex.match(/.{1,2}/g);
+    if (chunks) {
+        chunks.reverse();
+        const bigEndianHex = chunks.join('');
+        if (bigEndianHex.length % 2 !== 0) {
+            return '0x0' + bigEndianHex;
+        }
+        return '0x' + bigEndianHex;
+    }
+    return '';
+}
--- src/utils/oraclePriceProvider.ts
+++ src/utils/oraclePriceProvider.ts
@@ -1,4 +1,5 @@
 import {HttpProvider} from "@polkadot/api";
+import {convertStringValueToHexBigEndian} from "./encodersUtils";
 
 const { ApiPromise } = require('@polkadot/api');
 export class OraclePriceProvider {
@@ -6,27 +7,9 @@ export class OraclePriceProvider {
     public static async getLatestOraclePriceForMarketId(marketId : string) : Promise<BigInt> {
         const provider = new HttpProvider(process.env.NODE_RPC_URL);
         OraclePriceProvider.api = await ApiPromise.create({provider});
-        const value = OraclePriceProvider.convertStringValueToHexBigEndian(marketId)
+        const value = convertStringValueToHexBigEndian(marketId)
         const latestOraclePrice = await OraclePriceProvider.api.rpc.state.call('MarketApi_oracle_price', value);
         const optionType = OraclePriceProvider.api.createType('Option<Balance>', latestOraclePrice)
         return optionType.unwrapOr(0).toString();
-    }
-
-    private static convertStringValueToHexBigEndian(value : string) {
-        const bigIntValue = BigInt(value);
-        let littleEndianHex = bigIntValue.toString(16);
-        if (littleEndianHex.length % 2 !== 0) {
-            littleEndianHex = '0' + littleEndianHex;
-        }
-        const chunks = littleEndianHex.match(/.{1,2}/g);
-        if (chunks) {
-            chunks.reverse();
-            const bigEndianHex = chunks.join('');
-            if (bigEndianHex.length % 2 !== 0) {
-                return '0x0' + bigEndianHex;
-            }
-            return '0x' + bigEndianHex;
-        }
-        return '';
     }
 }
