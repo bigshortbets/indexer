2cb7dc0f6dfa347f57a16e028df30007cc6684b7
TomaszGol <oplane2000@gmail.com>
2023-12-07 11:36:53 +0100

    Back UnrealisedProfitLoseNetResolver

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,3 +1,5 @@
 import { UnrealisedProfitLoseNetResolver } from "./unrealisedProfitLoseNetResolver";
+import { OraclePriceResolver } from "./oraclePriceResolver";
 
 export { UnrealisedProfitLoseNetResolver };
+export { OraclePriceResolver };
