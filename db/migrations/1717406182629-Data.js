ad94b7c8340479b7268f137e508a01c07f68b0f3
Tomasz Gołkowski <tomaszgolkowski00@gmail.com>
2024-06-03 11:16:41 +0200

    Migration

--- db/migrations/1717406182629-Data.js
+++ db/migrations/1717406182629-Data.js
@@ -1,10 +1,5 @@
-<<<<<<<< HEAD:db/migrations/1717403923391-Data.js
-module.exports = class Data1717403923391 {
-    name = 'Data1717403923391'
-========
-module.exports = class Data1717405833883 {
-    name = 'Data1717405833883'
->>>>>>>> d960a6d (Add enum):db/migrations/1717405833883-Data.js
+module.exports = class Data1717406182629 {
+    name = 'Data1717406182629'
 
     async up(db) {
         await db.query(`CREATE TABLE "market" ("id" character varying NOT NULL, "ticker" text NOT NULL, "tick_size" numeric NOT NULL, "lifetime" numeric NOT NULL, "initial_margin" integer NOT NULL, "maintenance_margin" integer NOT NULL, "contract_unit" numeric NOT NULL, "block_height" numeric NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "daily_volume" numeric NOT NULL, "oracle_price" numeric, "status" character varying(5) NOT NULL, CONSTRAINT "PK_1e9a2963edfd331d92018e3abac" PRIMARY KEY ("id"))`)
