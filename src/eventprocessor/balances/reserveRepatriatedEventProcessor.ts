c3624e65f83e4baa201e513ff27c5d298a55c2ea
Tomasz Gołkowski <56382698+TomaszGol@users.noreply.github.com>
2024-06-07 12:57:57 +0200

    fix (#54)
    * fix
    
    * Fix brackets

--- src/eventprocessor/balances/reserveRepatriatedEventProcessor.ts
+++ src/eventprocessor/balances/reserveRepatriatedEventProcessor.ts
@@ -32,8 +32,10 @@ export class ReserveRepatriatedEventProcessor implements EventProcessor {
     const reserveRepatriatedEvent = events.balances.reserveRepatriated.v1;
     const found = block.events.find(
       (element) =>
-        element.index === event.index + 1 &&
-        element.extrinsicIndex === event.extrinsicIndex &&
+        (element.index === event.index + 1 ||
+          element.index === event.index + 2 ||
+          element.index === event.index + 3) &&
+        element.callAddress?.toString() === event.callAddress?.toString() &&
         (element.name === "Market.PositionMarkedToMarket" ||
           element.name === "Market.PositionReduced" ||
           element.name === "Market.PositionClosed"),
