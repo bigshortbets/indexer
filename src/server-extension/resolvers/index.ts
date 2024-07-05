f183399063ddd728f35803f2dabc0b8508a2f12e
Mkurowski03 <>
2024-07-05 18:42:46 +0200

    Resolve merge conflict markers

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,12 +1,6 @@
 import { UnrealisedProfitLoseNetResolver } from "./unrealisedProfitLoseNetResolver";
 import { OraclePriceResolver } from "./oraclePriceResolver";
-<<<<<<< Updated upstream
 import { MarginDataResolver } from "./marginDataResolver";
-
-export { UnrealisedProfitLoseNetResolver, OraclePriceResolver, MarginDataResolver };
-=======
 import { MarginResolver } from "./marginResolver";
-import { MarginDataResolver } from "./marginDataResolver";
-
 export { UnrealisedProfitLoseNetResolver, OraclePriceResolver, MarginDataResolver, MarginResolver };
->>>>>>> Stashed changes
+
