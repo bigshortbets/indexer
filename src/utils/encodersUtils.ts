ccbcbaccecbabeb7618d61d4450434073b3a22cb
Tomasz Gołkowski <tomaszgolkowski00@gmail.com>
2024-07-18 22:29:16 +0200

    Change coding

--- src/utils/encodersUtils.ts
+++ src/utils/encodersUtils.ts
@@ -7,7 +7,7 @@ export const encodeUserValue = (inputUserValue: string) => {
   } else {
     const uint8ArrayUserValue = new Uint8Array(
       // @ts-ignore
-      hexUserValue.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)),
+      hexUserValue.match(/.{1,2}/g).map((byte) => parseInt(byte, 16))
     );
     return ss58.codec(42).encode(uint8ArrayUserValue);
   }
@@ -34,50 +34,4 @@ export const convertStringValueToHexBigEndian = (value: string) => {
     return "0x" + bigEndianHex;
   }
   return "";
-};
-
-const LUT_HEX_4b = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-];
-const LUT_HEX_8b = new Array(0x100);
-for (let n = 0; n < 0x100; n++) {
-  LUT_HEX_8b[n] = `${LUT_HEX_4b[(n >>> 4) & 0xf]}${LUT_HEX_4b[n & 0xf]}`;
-}
-
-const bufferToHex = (buffer: Uint8Array) => {
-  buffer = new Uint8Array(buffer);
-  let out = "";
-  for (let idx = 0, edx = buffer.length; idx < edx; idx++) {
-    out += LUT_HEX_8b[buffer[idx]];
-  }
-  return "0x" + out.toLocaleLowerCase();
-};
-
-export const hexToLittleEndian = (hexValue: any) => {
-  hexValue = hexValue.startsWith("0x") ? hexValue.slice(2) : hexValue;
-  const hexBytes = hexValue.match(/.{1,2}/g);
-  const littleEndianBytes = hexBytes.reverse();
-  return "0x" + littleEndianBytes.join("");
-};
-
-export const convertToU8a = async (api: any, address: string) => {
-  const decodedAddress = api.createType("AccountId", address);
-
-  const u8aRepresentation = decodedAddress.toU8a(true);
-  return bufferToHex(u8aRepresentation);
 };
--- src/utils/marginDataProvider.ts
+++ src/utils/marginDataProvider.ts
@@ -1,14 +1,14 @@
 import { HttpProvider } from "@polkadot/api";
 import * as ss58 from "@subsquid/ss58-codec";
-import { hexToLittleEndian, convertToU8a } from "./encodersUtils";
+import { convertStringValueToHexBigEndian } from "./encodersUtils";
 const { ApiPromise } = require("@polkadot/api");
-import { nToHex } from "@polkadot/util";
+
 export class MarginDataProvider {
   private static api: any;
 
   public static async getMarginDataForMarket(
     marketId: string,
-    walletAddress: string,
+    walletAddress: string
   ): Promise<[number, string | null]> {
     const provider = new HttpProvider(process.env.NODE_RPC_URL);
     MarginDataProvider.api = await ApiPromise.create({
@@ -20,20 +20,24 @@ export class MarginDataProvider {
       },
     });
 
-    const addressPayload = (
-      await convertToU8a(MarginDataProvider.api, walletAddress)
-    ).substring(2);
+    const encodedMarketId = convertStringValueToHexBigEndian(marketId);
 
-    // Ensure the hexWalletAddress is used directly in the rpc call
+    let decodedWalletAddress = ss58.decode(walletAddress);
+
+    let hexWalletAddress = Buffer.from(decodedWalletAddress.bytes).toString(
+      "hex"
+    );
+
+    const dataBytes = encodedMarketId + hexWalletAddress;
     try {
       const marginData = await MarginDataProvider.api.rpc.state.call(
         "MarketApi_margin_data",
-        hexToLittleEndian(nToHex(BigInt(marketId))) + addressPayload,
+        dataBytes
       );
 
       const result = MarginDataProvider.api.createType(
         "(Balance, Option<(Balance, LiquidationStatus)>)",
-        marginData,
+        marginData
       );
 
       const jsonData = (result.toJSON() as [number, [number, string]?]) ?? [];
