8eae8e0c93d4dcfc3240bb9022f510d93e175c82
Tomasz Gołkowski <tomaszgolkowski00@gmail.com>
2024-07-23 14:07:23 +0200

    2TF Data

---
+++ src/model/generated/oracleChartFeed15Min.model.ts
@@ -0,0 +1,33 @@
+import {BigDecimal} from "@subsquid/big-decimal"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, BigDecimalColumn as BigDecimalColumn_, IntColumn as IntColumn_, Index as Index_, ManyToOne as ManyToOne_} from "@subsquid/typeorm-store"
+import {Market} from "./market.model"
+
+@Entity_()
+export class OracleChartFeed15Min {
+    constructor(props?: Partial<OracleChartFeed15Min>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @BigDecimalColumn_({nullable: false})
+    closePrice!: BigDecimal
+
+    @BigDecimalColumn_({nullable: false})
+    openPrice!: BigDecimal
+
+    @BigDecimalColumn_({nullable: false})
+    lowPrice!: BigDecimal
+
+    @BigDecimalColumn_({nullable: false})
+    highPrice!: BigDecimal
+
+    @Index_()
+    @IntColumn_({nullable: false})
+    timestamp!: number
+
+    @Index_()
+    @ManyToOne_(() => Market, {nullable: true})
+    market!: Market
+}
