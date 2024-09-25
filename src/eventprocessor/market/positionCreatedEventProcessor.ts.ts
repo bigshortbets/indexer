8b99db9aa003594c1a63c0349d1250fbfed6043a
Tomasz Gołkowski <56382698+TomaszGol@users.noreply.github.com>
2024-09-25 12:59:56 +0200

    Add market ohlc candle (#83)
    * Add market ohlc candle
    
    * Add migration
    
    * Fix typo

--- src/eventprocessor/market/positionCreatedEventProcessor.ts.ts
+++ src/eventprocessor/market/positionCreatedEventProcessor.ts.ts
@@ -1,6 +1,12 @@
 import { EventProcessor } from "../eventProcessor";
 import { Store } from "@subsquid/typeorm-store";
-import { Market, Position, PositionStatus } from "../../model";
+import {
+  MarketChartFeed15Min,
+  MarketChartFeed1H,
+  Market,
+  Position,
+  PositionStatus,
+} from "../../model";
 import * as events from "../../types/events";
 import {
   DataHandlerContext,
@@ -10,6 +16,7 @@ import {
 import { encodeUserValue } from "../../utils/encodersUtils";
 import { BigDecimal } from "@subsquid/big-decimal";
 import { USDC_DECIMALS } from "../../utils";
+import { updateCandle } from "../../utils/chartHelpers/timeframesBuilder";
 
 export class PositionCreatedEventProcessor implements EventProcessor {
   getHandledEventName(): string {
@@ -27,6 +34,7 @@ export class PositionCreatedEventProcessor implements EventProcessor {
       let parsedEvent = positionCreatedEvent.decode(event);
       let market = await ctx.store.get(Market, parsedEvent.market.toString());
       if (market) {
+        const price = BigDecimal(parsedEvent.price, USDC_DECIMALS);
         let position = new Position({
           id: parsedEvent.positionId.toString(),
           market: market,
@@ -38,12 +46,26 @@ export class PositionCreatedEventProcessor implements EventProcessor {
           timestamp: new Date(block.header.timestamp),
           status: PositionStatus.OPEN,
           quantityLeft: BigInt(parsedEvent.quantity),
-          createPrice: BigDecimal(parsedEvent.price, USDC_DECIMALS),
-          createPriceLong: BigDecimal(parsedEvent.price, USDC_DECIMALS),
-          createPriceShort: BigDecimal(parsedEvent.price, USDC_DECIMALS),
-          price: BigDecimal(parsedEvent.price, USDC_DECIMALS), // temporary - set in the next event
+          createPrice: price,
+          createPriceLong: price,
+          createPriceShort: price,
+          price: price, // temporary - set in the next event
         });
         await ctx.store.save(position);
+
+        // @ts-ignore
+        const timestamp = Math.floor(block.header.timestamp / 1000);
+        const rounded15Min = BigInt(Math.floor(timestamp / 900)) * BigInt(900);
+        const rounded1H = BigInt(Math.floor(timestamp / 3600)) * BigInt(3600);
+
+        await updateCandle(
+          ctx,
+          market,
+          price,
+          rounded15Min,
+          MarketChartFeed15Min,
+        );
+        await updateCandle(ctx, market, price, rounded1H, MarketChartFeed1H);
       } else {
         console.warn("Market undefined");
       }
